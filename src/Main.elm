c7c152becfc6b4a969d732015a48545c67d83cdb
Jan Wirth <jan@scalab.app>
2020-06-05 13:52:56 +0200

    Add content

--- src/Main.elm
+++ src/Main.elm
@@ -58,7 +58,8 @@ quote = Html.p [Html.Attributes.class "quote"] [
     , Html.span [Html.Attributes.class "ellipsis"] [Html.text "[...]"]
     , Html.text "more of the economy to essentially be about subjective values."
     , Html.span [Html.Attributes.class "ellipsis"] [Html.text "[...]"]
-    , Html.text "Because that's a signal that we are creating technologies and an economy that's really serving us."
+    , Html.text "Because that's a signal that we are creating technologies and an economy that's really "
+    , Html.span [Html.Attributes.class "nowrap"] [Html.text "serving us."]
     ]
 
 -- Jaron Lanier, Making Sense Podcast #135, minute 11
@@ -72,8 +73,11 @@ view model =
         -- intro
         [ h1 [Html.Attributes.class "title"] [ text "Jan Wirth - Artist Residency application"]
         , quote
+        , Html.p [Html.Attributes.class "quote-source"] [
+            Html.text "Jaron Lanier, Philosopher, Artist, Technologist"
+        ]
         , Html.p [Html.Attributes.class "intro-text"]
-            [ Html.text "My name is Jan. And you are reading this because after learning about the artist residency I just could not sleep. I could not sleep because I got very inspired."
+            [ Html.text "Hello There! My name is Jan. And you are reading this because after learning about the artist residency I just could not sleep. I could not sleep because I got very inspired."
             , Html.text "The general concept of the residency inspires me because I am passionate about technology and its implications for how we think, live and love."
             , Html.text "The themes perfectly match my profile. I am a creative technologist with a background in arts and design."
             , Html.text "Right now, you are looking at a mix between a non-stage live music experience and sound installation. It is composed, designed and hand-coded just for you."
@@ -86,10 +90,39 @@ view model =
             , Html.node "factory-beat-player" [Html.Attributes.attribute "state" (stateToString model.state)] []
         ]
         -- what I do
+        , Html.h2 [] [Html.text "Some Projects"]
+        , Html.p [Html.Attributes.class "maff"] [
+            Html.a [ Html.Attributes.href "https://github.com/FranzSkuffka/maff"] [Html.text "Maff"]
+           , Html.text " is a Telegram bot that helps you transcribe mathematical terms into LaTeX, reducing the need for scientist to learn special syntax for experession mathematical equations. "
+           , Html.text "Maff uses computer vision technology to do its job. I wrote it in on a misty sunday afternoon."
+           ]
+        , Html.div [Html.Attributes.class "img maff-picture"] [
+            Html.img [Html.Attributes.src "/maff.png"] []
+        ]
+
+        , Html.p [Html.Attributes.class "scalab-text"] [
+              Html.h2 [] [Html.text "Professional Experience"]
+            , Html.text "I am the CTO of "
+            , Html.a [Html.Attributes.href "https://scalab.app"] [Html.text "scalab.app"]
+            , Html.text ". We are working on technology that makes complex web app development accessible to more humans. "
+            , Html.text "Scalab is an open-source 'low-code' environment. "
+            , Html.text "You can design without code and evolve your work into a full web application. "
+            , Html.text "However, unlike other solutions, scalab is compatible with traditional, code-driven development workflows. "
+            , Html.text "That means everyone in a team that knows how to use tools like Microsoft Powerpoint can participate in the creation of the thing that gets shipped to the user "
+            , Html.text "I met my co-founder David Beesley through "
+            
+            , Html.a [Html.Attributes.href "https://joinef.com"] [Html.text "Entrepreneur First"]
+            , Html.text ", 'the world’s leading talent investor'."
+            , Html.p [] [
+                Html.text "Before Scalab I did work for "
+              , Html.a [Html.Attributes.href "https://mercedes-benz.io"] [Html.text "Mercedes-Benz.io"]
+              , Html.text " taking responsibility for the success of both customer-facing and internal tools. "
+              , Html.text "I fulfilled this responsibility in Stuttgart, Berlin, Lisbon and Singapore. "
+              , Html.text "I am grateful for this rich cultural and professional experience."
+            ]
+        ]
         -- scalab, music, arts
         -- what I did
-        -- ef, mercedes
-        -- talks in singapor and lisbon
         -- outro
         ]
 
